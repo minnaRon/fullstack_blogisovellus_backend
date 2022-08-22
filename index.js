323915e42046b38d87dec0b4b08273e7525cb8a8
Minna Ronka <minna.ronka@gmail.com>
2022-08-22 15:02:14 +0300

    Add blogsRouter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //tuodaan tiedostoon index.js:
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
+const blogsRouter = require('./controllers/blogs')
 
 //const http = require('http')
 const express = require('express') //
@@ -12,7 +12,6 @@ const mongoose = require('mongoose')
 
 
 //const Blog = mongoose.model('Blog', blogSchema)
-const Blog = require('./models/blog')
 
 //const mongoUrl = 'mongodb://localhost/bloglist'
 const mongoUrl = process.env.MONGODB_URI
@@ -21,23 +20,9 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+//tuodaan tiedostoon index.js
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
